4d225cb20ee0536fd3a1538241fecae255ef5bc8
troywein <45960380+troywein@users.noreply.github.com>
2021-09-29 12:02:18 -0600

    Update pex03funcs.h

--- pex03funcs.h
+++ pex03funcs.h
@@ -9,20 +9,20 @@
 
 #include <stdbool.h>
 
-void clearGameBoard(char board[8][8]);
+void clearGameBoard(char board[6][6]);
 
-void computerTakeTurn(char board[8][8]);
+void computerTakeTurn(char board[6][6]);
 
-void displayGameBoard(char board[8][8]);
+void displayGameBoard(char board[6][6]);
 
 int getIntRange(int lowVal, int highVal);
 
 int getIntSafe();
 
-bool legalPlay(char board[8][8], int column);
+bool legalPlay(char board[6][6], int column);
 
-void placePiece(char board[8][8], char piece, int column);
+void placePiece(char board[6][6], char piece, int column);
 
-void userTakeTurn(char board[8][8]);
+void userTakeTurn(char board[6][6]);
 
-int getWinner(char board[8][8]);
+int getWinner(char board[6][6]);
